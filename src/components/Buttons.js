b98f6c290b40613d1afc7687b4369a3194d55c69
Grant Glidewell <grantg@Grants-MacBook-Pro.local>
2017-07-29 22:33:11 -0700

    some work

---
+++ src/components/Buttons.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Buttons extends Component {
+  render (){
+    return(
+    <p>buttons</p>
+  )}
+}
+
+export default Buttons;
---
+++ src/components/Display.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Display extends Component {
+  render (){
+    return(
+    <p>display</p>
+  )}
+}
+
+export default Display;
